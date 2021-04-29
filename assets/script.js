89febe4232dc0ce70d79c9675ed12ba2591a9a43
ADAM KENNEDY <adamkennedy@MacBook-Air-2.local>
2021-04-28 19:16:08 -0700

    basic html, css, and JS files added

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// start here
+
+var artDiv = $("#artDiv");
+var infoDiv = $("#infoDiv");
+var musicDiv = $("#musicDiv");
+var randomArtBtn = $("#randomArtBtn");
+var searchArtBtn = $("#searchArtBtn");
+var searchMusicBtn = $("#searchMusicBtn");
+
+// var getArt = function() {
+    // var apiUrl = (URL HERE)
+
+    //fetch here
+// }
+
+//getArt()
